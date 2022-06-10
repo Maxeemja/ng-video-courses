598460c0c5e5b7cf9df163b5daeae5959ebc7bb7
Max <maxeemja.inc@gmail.com>
2022-06-10 14:12:10 +0300

    ht1 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LogoComponent } from './header/logo/logo.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    LogoComponent,
+    SearchBarComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
